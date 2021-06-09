432ed48b19ac5e7344a49e60080ac3dfc5bf631d
SaloniYerawar15 <77059802+SaloniYerawar15@users.noreply.github.com>
2021-06-09 22:25:36 +0530

    Create Python01.py
    Has 2 programs extension for file name and area for given radius..

---
+++ Python01.py
@@ -0,0 +1,16 @@
+// to calculate area
+
+from math import pi
+
+r = float(input ("Enter radius of circle : "))
+
+print ("Area of the circle is: " + str(pi * r**2))
+
+
+
+
+// add extension to file name
+
+fn= input("Enter Filename: ")
+f = fn.split(".")
+print ("Extension of the file is : " + f[-1])
